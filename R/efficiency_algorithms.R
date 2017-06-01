98ac01f576a060c1dd2ba8d4d3268ae8a22ed494
Frits Traets <frits.traets@kuleuven.be>
2017-06-01 18:01:02 +0200

    Changed Modfed and SeqDB functions
    updated documentation

--- R/efficiency_algorithms.R
+++ R/efficiency_algorithms.R
@@ -2,88 +2,108 @@
 
 #' Modified Federov algorithm for MNL models.
 #' 
-#' The algorithm swipes every profile of an initial random design with candidate
-#' profiles. By doing this it tries to minimize the D(B) error, assuming
+#' The algorithm swipes every profile of an initial design with candidate
+#' profiles. By doing this it tries to minimize the D(B)-error, assuming
 #' a multinomial logit model. See reference for more information.  
 #' 
 #' The algorithm stops when an iteration occured without replacing a profile or 
 #' when \code{max.iter} is reached. An iteration is a loop through all profiles 
-#' from the initial design, testing every profile from the candidate design.
+#' from the initial design, evaluating the change in D(B)-error for every
+#' profile from \code{cand.set}.
 #' 
 #' By specifying a numeric vector in \code{par.samples}, the D-error will be 
-#' calculated and the design will be optimised locally. By specifying a matrix 
-#' in which each row is a sample from a multivariate distribution in 
-#' \code{par.samples}, the DB-error will be calculated, and the design will be 
-#' optimised globally. The number of columns should equal the number of
-#' parameters for alternative specific constants (\code{alt.cte}) + the number
-#' of attribute parameters (\code{ncol(cand.des)}). This is also the order in
-#' which they should appear (first \code{alt.cte} parameters).
+#' calculated and the design will be optimised locally. By specifying a matrix, 
+#' in which each row is a sample from a multivariate distribution, the
+#' DB-error will be calculated, and the design will be optimised globally. The
+#' number of columns should equal the number of parameters needed for
+#' \code{alt.cte} + the number of parameters needed for \code{cand.set}. This is
+#' also the order in which they should be sorted (first \code{alt.cte}
+#' parameters).
+#' 
+#' The DB-error is calculated by taking the mean over
+#' D-errors. It could be that for some samples the design results in an infinite
+#' D-error. The percentage of samples for which this was true for the final
+#' design can be found in the output \code{inf.error}.
 #' 
 #' Alternative specific constants can be specified in \code{alt.cte}. The lenght
-#' of this binary vector should equal \code{n.alts}, were a zero indicates no
-#' alternative specific constant and a one the opposit.
+#' of this binary vector should equal \code{n.alts}, were 0 indicates no
+#' alternative specific constant and 1 the opposit.
 #' 
-#' @param cand.set A numeric matrix in which each row is a possible profile. See
-#'   \code{\link{Profiles}}
+#' @param cand.set A numeric matrix in which each row is a possible profile. The
+#'   \code{\link{Profiles}} function can be used to generate this. 
 #' @param n.sets Numeric value indicating the number of choice sets.
 #' @param n.alts Numeric value indicating the number of alternatives per choice 
 #'   set.
-#' @param par.samples A matrix in which each row is a sample from the 
-#'   multivariate prior parameter distribution.
 #' @param alt.cte A binary vector indicating for each alternative if an alternative 
 #'   specific constant is desired.
+#' @param par.samples A matrix in which each row is a sample from the 
+#'   multivariate prior parameter distribution.
 #' @param start.des A matrix in which each row is a profile. The number of rows
 #'   equals \code{n.sets * n.alts}, and the number of columns equals the number
-#'   of columns of \code{par.samples}
+#'   of columns of \code{cand.set}
 #' @param max.iter A numeric value indicating the maximum number allowed 
 #'   iterations.
 #' @return 
-#' \item{design}{An efficient design.}
-#' \item{error}{The D(B)-error of the design.}
-#' \item{Na.percentage}{The percentage of
+#' \item{design}{A matrix wich represents an efficient design.}
+#' \item{error}{Numeric value indicating the D(B)-error of the design.}
+#' \item{inf.error}{Numeric value indicating the percentage of
 #'  samples for which the D-error was \code{Inf}.}
-#' \item{prob.diff}{The difference between the alternative with
+#' \item{prob.diff}{Numeric value indicating the difference between the alternative with
 #' the highest and the one with the lowest probability for each choice set. If a
 #' sample matrix was provided this is based on the mean over all samples.}
+#' @examples
+#' # D-efficient design
+#' # 3 Attributes, 2 are dummy coded and 1 continuous (= 3 parameters).
+#' cs <- Profiles(lvls = c(2, 3, 2), coding = c("D", "C", "D"), c.lvls = list(c(2,4,6)))
+#' ps <- c(0.8, 0.2, -0.3) # Prior parameter vector
+#' Modfed(cand.set = cs, n.sets = 8, n.alts = 2, alt.cte = c(0,0), par.samples = ps)
+#' 
+#' # DB-efficient design. 
+#' # 3 Attributes with 2, 3 and 2 levels, all effect coded (= 4 parameters).
+#' cs <- Profiles(lvls = c(2, 3, 2), coding = c("E", "E", "E")) 
+#' m <- c(0.8, 0.2, -0.3, -0.2, 0.7) # Prior mean (total = 5 parameters).
+#' v <- diag(length(m)) # Prior variance. 
+#' ps <- MASS::mvrnorm(n = 10, mu = m, Sigma = v) # 10 Samples.
+#' Modfed(cand.set = cs, n.sets = 8, n.alts = 2, alt.cte = c(1,0), par.samples = ps)
 #' @export
-Modfed <- function(cand.set, n.sets, n.alts, par.samples, alt.cte, start.des = NULL, max.iter = Inf) {
-  # handling par.samples
+Modfed <- function(cand.set, n.sets, n.alts,  alt.cte, par.samples, start.des = NULL, max.iter = Inf) {
+  # Handling par.samples.
   if (!(is.matrix(par.samples))) {
     par.samples <- matrix(par.samples, nrow = 1)
   }
-  # error alternative specific constants 
+  # Error alternative specific constants. 
   if (length(alt.cte) != n.alts) {
     stop("n.alts does not match the alt.cte vector")
   }
-  # error identifying model
+  # Error identifying model.
   if (n.sets < ncol(par.samples)) {
     stop("Model is unidentified. Increase the number of choice sets or decrease parameters to estimate.")
   }
-  # errors start design 
+  # Errors start design. 
   if (!is.null(start.des)) {
     if (ncol(start.des) != ncol(cand.set))  {
-      stop("Number of colums start design is different from number of columns candidate set.")
+      stop("number of colums start design is different from number of columns candidate set.")
     }
     if (nrow(start.des) != (n.alts * n.sets)) {
-      stop("Number of rows start design is different from number of sets times number of alternatives.")
+      stop("number of rows start design is different from number of sets times number of alternatives.")
     }
   }
-  # create alternative specific design
+  # Create alternative specific design.
   cte.des <- Altspec(alt.cte = alt.cte, n.sets = n.sets)
   # error handling
   if (ncol(cand.set)+ ncol(cte.des) != ncol(par.samples)) {
-    stop("dimension of par.samples does not match the dimension of cand.set + alt.cte")
+    stop("dimension of par.samples does not match the dimension of alt.cte + cand.set.")
   }
-  # random start design
+  # Random start design.
   if (!is.null(start.des)) {
     des <- start.des
   } else {
     r <- round(runif((n.sets*n.alts), 1, nrow(cand.set)))
     des <- data.matrix(cand.set[r, ])
   }
-  # combine with alt.spec design
+  # Combine with alt.spec design.
   des <- cbind(cte.des, des)
-  # starting values 
+  # Starting values. 
   d.start <- apply(par.samples, 1, Derr, des = des,  n.alts = n.alts)
   db.start <- mean(d.start, na.rm = TRUE)
   converge <- FALSE
@@ -92,109 +112,169 @@ Modfed <- function(cand.set, n.sets, n.alts, par.samples, alt.cte, start.des = N
   n.samples <- nrow(par.samples)
   n.cte <- ncol(cte.des)
   n.par <- ncol(des)
-  # start algorithm
+  # start algorithm.
   while (!converge & it <= max.iter) {
     it <- it + 1
-    # show progress iteration
+    # show progress iteration.
     pb <- txtProgressBar(min = 0, max = nrow(des), style = 3)     
-    # save design before iteration
+    # save design before iteration.
     iter.des <- des
-    # for every row in the design
+    # For every row in the design.
     for (r in 1:nrow(des)) {
-      # switch with everey row in candidate set 
+      # Switch with everey row in candidate set. 
       for (c in 1:nrow(cand.set)) {
         des[r, (n.cte + 1) : n.par ] <- cand.set[c, ]
-        # calculate d errors
+        # Calculate D-errors.
         d.errors <- apply(par.samples, 1, Derr, des = des,  n.alts = n.alts)
-        #db error 
+        # DB-error. 
         db <- mean(d.errors, na.rm = TRUE)
-        # change if lower db error
+        # Change if lower db error.
         if (db < db.start) {
           best.row <- as.numeric(des[r, ])
           db.start <- db
           change <- TRUE
         }
-        # show progress iteration
+        # Show progress iteration.
         setTxtProgressBar(pb, r)
       }
-      # replace with best profile if change
+      # Replace with best profile if change.
       if (change) {
         des[r, ] <- best.row
       } else {
         des[r, ] <- iter.des[r, ]
       }
-      # initialize variables again 
+      # Initialize variables again. 
       change <- FALSE
       na.percentage <- 0
     }
-    close(pb)  # print progress 
-    converge <- isTRUE(all.equal(des, iter.des)) # convergence if no profile is swapped this iteration 
+    close(pb)  # Print progress. 
+    converge <- isTRUE(all.equal(des, iter.des)) # Convergence if no profile is swapped this iteration.
   }
-  # calculate percentage NA values
+  # calculate percentage NA values.
   d.errors <- apply(par.samples, 1, Derr, des = des,  n.alts = n.alts)
   if (any(is.na(d.errors))) {
     na.percentage <- percent(sum(is.na(d.errors))/n.samples)
   } 
-  # utility balance
+  # Utility balance.
   ub <- apply(par.samples, 1, Utbal, des = des,  n.alts = n.alts)
   ub <- .rowMeans(ub, m = n.sets, n = n.samples, na.rm = FALSE)
-    # rownames design 
+    # Rownames design. 
   des.names <- Rcnames(n.sets = n.sets, n.alts = n.alts, n.cte = n.cte, alt.cte = alt.cte)
   rownames(des) <- des.names[[1]]
-  # colnames alternative specific constants 
+  # Colnames alternative specific constants. 
   if (n.cte != 0) {
     colnames(des)[1:n.cte] <- des.names[[2]]
   }
-  # return design, D(B)error, percentage NA's, utility balance. 
-  return(list("design" = des, "error" =  db, "Na.percentage" = na.percentage, "prob.diff" = ub))
+  # Return design, D(B)error, percentage NA's, utility balance. 
+  return(list("design" = des, "error" =  db, "inf.error" = na.percentage, "prob.diff" = ub))
 }
 
 
-#' DB sequential set
+#' Sequential modified federov algorithm for MNL model.
 #' 
-#' Adds the most DB efficient choice set to a design, given (updated) parameter
+#' Selects the choice set that minimizes the DB-error when added to an initial design, given (updated) parameter
 #' values.
-#' @param des A design matrix in which each row is a profile.
-#' @param cand.set A numeric matrix in which each row is a possible profile.
-#' @param n.alts Numeric value indicating the number of alternatives per choice
-#'   set.
-#' @param par.samples A matrix in which each row is a sample from the
-#'   multivariate parameter distribution.
-#' @param weights A vector containing the weights of the samples.
+#' 
+#' This algorithm is ideally used in an adaptive context. The algorithm will
+#' select the next DB-efficient choice set given parameter values and an initial
+#' design. In an adaptive context these parameter values are updated after each
+#' observed response.
+#' 
+#' The initial design \code{des} can be generated with \code{\link{Modfed}}. If
+#' alternative specific constants are included in the initial design, the
+#' algorithm will use the same for selecting the new choice set. Columns of
+#' \code{des} which contain ".cte" in their name are recognized as alternative
+#' specific columns.
+#' 
+#' The list of potential choice sets are created using
+#' \code{\link[gtools]{combinations}}. If \code{reduce} is \code{TRUE},
+#' \code{repeats.allowed = FALSE} and vice versa.
+#' If no alternative constants are used \code{reduce} should always be \code{TRUE}.
+#' When alternative specific constants are used \code{reduce} can be \code{TRUE}
+#' so that the algorithm will be faster, but the combinations of constants
+#' and profiles will not be evaluated exhaustively.
+#' 
+#' The \code{weights} can be used when the \code{par.samples} have weights. This
+#' is for example the case when parameter values are updated using
+#' \code{\link{ImpSamp}}.
+#' @inheritParams Modfed
+#' @param des A design matrix in which each row is a profile. Can be generated with \code{\link{Modfed}}
 #' @param prior.covar Covariance matrix of the prior distribution.
-#' @return The most db efficient choice set.
+#' @param reduce Logical value indicating whether the candidate set should be reduced or not. 
+#' @param weights A vector containing the weights of the samples. Default is \code{NULL}
+#' @return 
+#' \item{set}{A matrix representing a DB efficient choice set.}
+#' \item{db.error}{A numeric value indicating the DB-error of the whole design.}
+#' @examples 
+#' # DB efficient choice set given design and parameter samples. 
+#' # Candidate profiles 
+#' cs <- Profiles(lvls = c(3, 3), coding = c("E", "E"))
+#' m <- c(0.3, 0.2, -0.3, -0.2) # Prior mean (total = 5 parameters).
+#' pc <- diag(length(m)) # Prior variance
+#' ps <- MASS::mvrnorm(n = 10, mu = m, Sigma = pc) # 10 Samples.
+#' ac <- c(0, 0) # No alternative specific constants. 
+#' # Initial design.
+#' des <- Modfed(cand.set = cs, n.sets = 6, n.alts = 2, alt.cte = ac, par.samples = ps)$design
+#' # Efficient choice set to add. 
+#' SeqDB(des = des, cand.set = cs, n.alts = 2, par.samples = ps, prior.covar = pc, reduce = TRUE)
+#' 
+#' # DB efficient choice set given design and parameter samples. 
+#' # Candidate profiles 
+#' cs <- Profiles(lvls = c(3, 3), coding = c("C", "E"), c.lvls = list(c(5,3,1)))
+#' m <- c(0.7, 0.3, -0.3, -0.2) # Prior mean (total = 5 parameters).
+#' pc <- diag(length(m)) # Prior variance
+#' ps <- MASS::mvrnorm(n = 10, mu = m, Sigma = pc) # 10 Samples.
+#' ac <- c(1, 0) # Alternative specific constant. 
+#' # Initial design.
+#' des <- Modfed(cand.set = cs, n.sets = 6, n.alts = 2, alt.cte = ac, par.samples = ps)$design
+#' # Efficient choice set to add. 
+#' SeqDB(des = des, cand.set = cs, n.alts = 2, par.samples = ps, prior.covar = pc, reduce = TRUE)
 #' @export
-seqfed.db <- function(des, cand.set, n.alts, par.samples, weights, prior.covar) {
-
-  #start values
-  db_best<-1000
-  full_comb<- full_sets(cand.set, n.alts)
-
-  #for each potential set:
-  for (p in 1:nrow(cand.set)){
-
-    set<-as.matrix(cand.set[as.numeric(full_comb[p, ]), ])
-
-    #for each par draw calculate d-error:
-    db_error<-0
-
-    for (s in 1: nrow(par.samples)){
-
-      info_s<-info_design(par = par.samples[s, ], des = set, n.alts = n.alts)
-      info_d<-info_design(par= par.samples[s, ], des = des, n.alts = n.alts)
-      inv_cov_prior<-solve(prior.covar)
-
-      db_error<- db_error+(det(info_d + info_s + inv_cov_prior)*(-1/ncol(par.samples))*weights[s])
-
-    }
-
-    #select new set if smaller db error
-    if (db_error< db_best){final_set<-set; db_best<- db_error}
+SeqDB <- function(des, cand.set, n.alts, par.samples, prior.covar, reduce = TRUE, weights = NULL) {
+  # Initialize.
+  n.sets <- nrow(des) / n.alts
+  cte.des <- NULL
+  # If no weights, equal weights.
+  if (is.null(weights)) {
+    weights <- rep(1, nrow(par.samples))
   }
-
-  return(final_set)
-
+  # Detect alternative specific constants
+  des.f <- as.data.frame(des)
+  alt.cte <- select(des.f, contains(".cte"))
+  if (ncol(alt.cte) > 0) {
+    cte.des <- alt.cte[1:n.alts, ]
+  }
+  # Handling par.samples.
+  if (!(is.matrix(par.samples))) {
+    par.samples <- matrix(par.samples, nrow = 1)
+  }
+  # Error par.samples
+  if (ncol(des) != ncol(par.samples)) {
+    stop("Numbers of parameters in par.samples does not match the number of parameters in the design.")
+  }
+  # Error identifying model.
+  if (n.sets < ncol(par.samples)) {
+    stop("Model is unidentified. Increase the number of choice sets or decrease parameters to estimate.")
+  }
+  # Starting and initializing values.
+  i.cov <- solve(prior.covar)
+  d.start <- apply(par.samples, 1, Derr, des = des,  n.alts = n.alts)
+  db.start <- mean(d.start, na.rm = TRUE)
+  full.comb <- combinations(n = nrow(cand.set), r = n.alts, repeats.allowed = !reduce)
+  n.par <- ncol(par.samples)
+  # For each potential set, select best. 
+  db.errors <- apply(full.comb, 1, DBerrS, cand.set, par.samples, des, n.alts, cte.des, i.cov, n.par, weights)
+  comb.nr <- as.numeric(full.comb[which.min(db.errors), ])
+  set <- cand.set[comb.nr, ]
+  # Add alternative specific constants if necessary
+  if (!is.null(cte.des)) {
+    set <- cbind(cte.des, set)
+  }
+  db <- min(db.errors)
+  #return best set and db error design.
+  return(list(set = set, db.error = db))
 }
+
 
 #' KL set selecting
 #' 
