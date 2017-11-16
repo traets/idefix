60136a0d8e1982567caef82ad5bcf0a49babcaba
Frits Traets <frits.traets@kuleuven.be>
2017-11-16 14:15:40 +0100

    included data description

---
+++ R/data.R
@@ -0,0 +1,13 @@
+#' Discrete choice design.
+#' 
+#' This discrete choice design is generated using the \code{\link{Modfed}} 
+#' function. There are 8 choice sets, each containig 2 alternatives (rows). The
+#' alternatives consist of 3 attributes (time, price, comfort) with each 3
+#' levels, all of which are dummy coded (columns).
+#' 
+#' @docType data
+#'   
+#' @usage data(design)
+#'   
+#' @keywords datasets
+"design"
