9df1c2f4209329c83b6af1ac788336f92f31e3aa
Frits Traets <frits.traets@kuleuven.be>
2018-09-25 13:56:17 +0200

    added extra example design

--- R/data.R
+++ R/data.R
@@ -7,9 +7,24 @@
 #' 
 #' @docType data
 #' @usage data(example_design)
-#' @format A matrix with 16 rows and 6 variables
+#' @format A matrix with 16 rows and 6 columns.
 #' @keywords data
 "example_design"
+
+
+#' Discrete choice design.
+#' 
+#' This discrete choice design is generated using the \code{\link{Modfed}} 
+#' function. There are 8 choice sets, each containig 3 alternatives (rows). The
+#' alternatives consist of 3 attributes (time, price, comfort) with each 3 
+#' levels, all of which are dummy coded (columns). The first two colums are
+#' alternative specific constants for alternative 1 and 2.
+#' 
+#' @docType data
+#' @usage data(example_design2)
+#' @format A matrix with 24 rows and 8 columns.
+#' @keywords data
+"example_design2"
 
 
 #' Discrete choice aggregate design.
