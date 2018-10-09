bb434a45d947d8879fefc570ce5424e5096e7510
Frits Traets <frits.traets@kuleuven.be>
2018-10-09 15:58:33 +0200

    specified namespace

--- R/Fullsets_ucpp.R
+++ R/Fullsets_ucpp.R
@@ -4,10 +4,10 @@ Fullsets_ucpp <- function(cand.set, n.alts, no.choice, reduce = TRUE){
   if(!is.null(no.choice)){
     n.alts <- n.alts - 1
   }
-  full.comb <- combn(1:nrow(cand.set), n.alts, FUN = function(x)  cand.set[x, ], simplify = FALSE)
+  full.comb <- utils::combn(1:nrow(cand.set), n.alts, FUN = function(x)  cand.set[x, ], simplify = FALSE)
   #reduce
   if (reduce){
-    m <- rnorm(ncol(cand.set))
+    m <- stats::rnorm(ncol(cand.set))
     inf <-list()
     for(i in 1:length(full.comb)){
       inf[[i]] <- round(InfoDes_cpp(m, full.comb[[i]], n.alts), digits = 3)
@@ -20,3 +20,4 @@ Fullsets_ucpp <- function(cand.set, n.alts, no.choice, reduce = TRUE){
   }
   return(full.comb)
 }
+
