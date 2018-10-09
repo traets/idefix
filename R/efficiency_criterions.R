4a1e461c49dc3527a6739a94d1f47b194bbcb02a
Frits Traets <frits.traets@kuleuven.be>
2018-10-09 13:36:34 +0200

    KL info under construction

--- R/efficiency_criterions.R
+++ R/efficiency_criterions.R
@@ -116,47 +116,47 @@ Utbal <- function(par, des, n.alts) {
   return(ub)
 }
 
-# KL information
-# 
-# Calculates the Kullback-Leibler divergence for all posible choice sets, given
-# @inheritParams SeqDB
-# @param full.comb A matrix in which each row is a possible combination of
-#   profiles.
-# @return Numeric value indicating the Kullback-Leibler divergence.
-KLs <- function(full.comb, par.draws, cte.des, cand.set, weights) {
-  #take set
-  set <- as.matrix(cand.set[as.numeric(full.comb), ])
-  #Alternative specific constants 
-  set <- cbind(cte.des, set)
-  #calculate for all sets the KLinfo.
-  kl <- KL(set, par.draws, weights)
-  return(kl)
-}
+# # KL information
+# # 
+# # Calculates the Kullback-Leibler divergence for all posible choice sets, given
+# # @inheritParams SeqDB
+# # @param full.comb A matrix in which each row is a possible combination of
+# #   profiles.
+# # @return Numeric value indicating the Kullback-Leibler divergence.
+# KLs <- function(full.comb, par.draws, cte.des, cand.set, weights) {
+#   #take set
+#   set <- as.matrix(cand.set[as.numeric(full.comb), ])
+#   #Alternative specific constants 
+#   set <- cbind(cte.des, set)
+#   #calculate for all sets the KLinfo.
+#   kl <- KL(set, par.draws, weights)
+#   return(kl)
+# }
 
 
-# Kullback-Leibler divergence for a set
-# 
-# Calculates the KL-divergence for a choice set given parameter values.
-# @inheritParams DerrS
-# @param weights A vector containing the weights of the draws. Default is
-#   \code{NULL}
-KL <- function (set, par.draws, weights){
-  # Probabilities.
-  num2 <- tcrossprod(set, par.draws)
-  mmat2 <- as.matrix(t(apply(num2, 2, max)))
-  numm2 <- exp(sweep(num2, 2, mmat2, FUN = "-"))
-  nummax <- exp(sweep(num2, 2, mmat2, FUN = "-"))
-  denom <- colSums(numm2)
-  ps <- sweep(nummax, 2, denom, FUN = "/")
-  lgp <- log(ps)
-  wprob <- sweep(ps, 2, weights, FUN="*")
-  twp <- rowSums(wprob)
-  lgwp <- sweep(lgp, 2, weights, FUN="*")
-  tlwp <- rowSums(lgwp)
-  #kullback Leibler information
-  klinfo <- sum(twp * (log(twp) - tlwp))
-  return (as.numeric(klinfo))
-}
+# # Kullback-Leibler divergence for a set
+# # 
+# # Calculates the KL-divergence for a choice set given parameter values.
+# # @inheritParams DerrS
+# # @param weights A vector containing the weights of the draws. Default is
+# #   \code{NULL}
+# KL <- function (set, par.draws, weights){
+#   # Probabilities.
+#   num2 <- tcrossprod(set, par.draws)
+#   mmat2 <- as.matrix(t(apply(num2, 2, max)))
+#   numm2 <- exp(sweep(num2, 2, mmat2, FUN = "-"))
+#   nummax <- exp(sweep(num2, 2, mmat2, FUN = "-"))
+#   denom <- colSums(numm2)
+#   ps <- sweep(nummax, 2, denom, FUN = "/")
+#   lgp <- log(ps)
+#   wprob <- sweep(ps, 2, weights, FUN="*")
+#   twp <- rowSums(wprob)
+#   lgwp <- sweep(lgp, 2, weights, FUN="*")
+#   tlwp <- rowSums(lgwp)
+#   #kullback Leibler information
+#   klinfo <- sum(twp * (log(twp) - tlwp))
+#   return (as.numeric(klinfo))
+# }
 
 
 
