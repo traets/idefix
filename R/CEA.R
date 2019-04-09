1100ca17b994b3965819ed081949ab461081e61d
Daniel Gil <danielgils@gmail.com>
2019-04-09 11:35:51 +0200

    Implementing SeqCEA

--- R/CEA.R
+++ R/CEA.R
@@ -477,3 +477,329 @@ CEAcore_ucpp <- function(des, par.draws, levels.list, n.alts, n.sets, n.cte,
   return(list("design" = des, "error" =  db.start, "inf.error" = na.percentage,
               "probs" = pmat))
 }
+
+
+#' Sequential coordinate exchange algorithm for MNL model.
+#' 
+#' Selects the choice set that minimizes the DB-error when added to an initial 
+#' design, given (updated) parameter values.
+#' 
+#' This algorithm is ideally used in an adaptive context. The algorithm will 
+#' select the next DB-efficient choice set given parameter values and possible
+#' previously generated choice sets. In an adaptive context these parameter
+#' values are updated after each observed response.
+#' 
+#' Previously generated choice sets, which together form an initial design, can
+#' be provided in \code{des}. When no design is provided, the algorithm will
+#' select te most efficient choice set based on the fisher information of the
+#' prior covariance matrix \code{prior.covar}.
+#' 
+#' If \code{alt.cte = NULL}, \code{par.draws} should be a matrix in which each 
+#' row is a sample from the multivariate parameter distribution. In case that 
+#' \code{alt.cte} is not \code{NULL}, a list containing two matrices should be 
+#' provided to \code{par.draws}. The first matrix containing the parameter draws
+#' for the alternative specific parameters. The second matrix containing the
+#' draws for the rest of the parameters.
+#' 
+#' The list of potential choice sets are created using 
+#' \code{\link[gtools]{combinations}}. If \code{reduce} is \code{TRUE}, 
+#' \code{repeats.allowed = FALSE} and vice versa. Furthermore, the list of 
+#' potential choice sets will be screaned in order to select only those choice 
+#' sets with a unique information matrix. If no alternative specific constants are used, 
+#' \code{reduce} should always be \code{TRUE}. When alternative specific 
+#' constants are used \code{reduce} can be \code{TRUE} so that the algorithm 
+#' will be faster, but the combinations of constants and profiles will not be 
+#' evaluated exhaustively.
+#' 
+#' The \code{weights} argument can be used when the \code{par.draws} have 
+#' weights. This is for example the case when parameter values are updated using
+#' \code{\link{ImpsampMNL}}.
+#' 
+#' When \code{parallel} is \code{TRUE}, \code{\link[parallel]{detectCores}} will
+#' be used to decide upon the number of available cores. That number minus 1 
+#' cores will be used to search for the optimal choice set. For small problems 
+#' (6 parameters), \code{parallel = TRUE} can be slower. For larger problems the
+#' computation time will decrease significantly.
+#' @inheritParams Modfed
+#' @param par.draws A matrix or a list, dependend on \code{alt.cte}. 
+#' @param des A design matrix in which each row is a profile. If alternative 
+#'   specific constants are present, those should be included as the first 
+#'   column(s) of the design. Can be generated with \code{\link{Modfed}}
+#' @param prior.covar Covariance matrix of the prior distribution.
+#' @param weights A vector containing the weights of the draws. Default is 
+#'   \code{NULL}, See also \code{\link{ImpsampMNL}}.
+#' @param parallel Logical value indicating whether computations should be done 
+#'   over multiple cores.
+#' @param no.choice An integer indicating the no choice alternative. The default
+#'   is \code{NULL}.
+#' @param reduce Logical value indicating whether the candidate set should be 
+#'   reduced or not.
+#' @return \item{set}{A matrix representing a DB efficient choice set.} 
+#'   \item{error}{A numeric value indicating the DB-error of the whole 
+#'   design.}
+#' @importFrom Rdpack reprompt
+#' @references \insertRef{ju}{idefix}
+#' @examples 
+#' # DB efficient choice set, given a design and parameter draws. 
+#' # Candidate profiles 
+#' cs <- Profiles(lvls = c(3, 3, 3), coding = c("E", "E", "E"))
+#' m <- c(0.3, 0.2, -0.3, -0.2, 1.1, 2.4) # mean (total = 6 parameters).
+#' pc <- diag(length(m)) # covariance matrix
+#' set.seed(123)
+#' sample <- MASS::mvrnorm(n = 10, mu = m, Sigma = pc)
+#' # Initial design.
+#' des <- example_design 
+#' # Efficient choice set to add. 
+#' SeqDB(des = des, cand.set = cs, n.alts = 2, par.draws = sample, 
+#'            prior.covar = pc, parallel = FALSE)
+#' 
+#' # DB efficient choice set, given parameter draws. 
+#' # with alternative specific constants 
+#' des <- example_design2 
+#' cs <- Profiles(lvls = c(3, 3, 3), coding = c("E", "E", "E"))
+#' ac <- c(1, 1, 0) # Alternative specific constants. 
+#' m <- c(0.3, 0.2, -0.3, -0.2, 1.1, 2.4, 1.8, 1.2) # mean 
+#' pc <- diag(length(m)) # covariance matrix
+#' pos <- MASS::mvrnorm(n = 10, mu = m, Sigma = pc)
+#' sample <- list(pos[ , 1:2], pos[ , 3:8])
+#' # Efficient choice set. 
+#' SeqDB(des = des, cand.set = cs, n.alts = 3, par.draws = sample, alt.cte = ac, 
+#'            prior.covar = pc, parallel = FALSE)
+#' @export
+SeqCEA <- function(des = NULL, lvls, coding, c.lvls = NULL, n.alts, par.draws, 
+                   prior.covar, n.cs = NULL, alt.cte = NULL, no.choice = NULL,
+                   weights = NULL,
+                   parallel = TRUE, reduce = TRUE) {
+  # Error handling initial design
+  if (is.null(des)) {
+    n.sets <- 1L
+  } else { 
+    if (!is.matrix(des)) {
+      stop("'des' should be a matrix or NULL")
+    }
+    if (!isTRUE(nrow(des) %% n.alts == 0)) {
+      stop("'n.alts' does not seem to match with the number of rows in 'des'")
+    }
+    n.sets <- nrow(des) / n.alts
+  }
+  
+  ### Error handling for design specifications
+  # No choice errors
+  if (!is.null(no.choice)) {
+    if (!is.wholenumber(no.choice)) {
+      stop("'no.choice' should be an integer or NULL")
+    }
+    if (any(isTRUE(no.choice > (n.alts + 0.2)), isTRUE(no.choice < 0.2))) {
+      stop("'no.choice' does not indicate one of the alternatives")
+    }
+    if (is.null(alt.cte)) {
+      stop("if there is a no choice alternative, 'alt.cte' should be specified")
+    }
+    if (!isTRUE(all.equal(alt.cte[no.choice], 1))) {
+      stop("the no choice alternative should correspond with a 1 in 'alt.cte'")
+    }
+  }
+  
+  # Alternative constant errors
+  if (!is.null(alt.cte)) {
+    if (length(alt.cte) != n.alts) {
+      stop("'n.alts' does not match the 'alt.cte' vector")
+    }
+    if (!all(alt.cte %in% c(0, 1))) {
+      stop("'alt.cte' should only contain zero or ones.")
+    }
+    # alternative specific constants
+    n.cte <- length(which(alt.cte == 1L))
+    if (isTRUE(all.equal(n.cte, 0L))) {
+      alt.cte <- NULL
+      cte.des <- NULL
+    }
+    
+    # Handling errors when there are alternative constants
+    if (n.cte > 0.2) {
+      if (!is.list(par.draws)) {
+        stop("'par.draws' should be a list when 'alt.cte' is not NULL")
+      }
+      if (!isTRUE(all.equal(length(par.draws), 2))) {
+        stop("'par.draws' should contain two components")
+      }
+      # If there is only one specific constant and is a vector, then it is
+      # transformed to a matrix
+      if (isTRUE(all.equal(n.cte, 1))) {
+        if (is.vector(par.draws[[1]])) {
+          par.draws[[1]] <- matrix(par.draws[[1]], ncol = 1)
+        }
+      }
+      if (!(all(unlist(lapply(par.draws, is.matrix))))) {
+        stop("'par.draws' should contain 2 matrices")
+      }
+      if (!isTRUE(all.equal(ncol(par.draws[[1]]), n.cte))) {
+        stop("the first component of 'par.draws' should contain the same number 
+             of columns as there are non zero elements in 'alt.cte'")
+      }
+      dims <-  as.data.frame(lapply(par.draws, dim))
+      if (!isTRUE(all.equal(dims[1, 1], dims[1, 2]))) { 
+        stop("the number of rows in the components of 'par.draws' should be equal")
+      }
+      par.draws  <- do.call("cbind", par.draws) # Transform par.draws to a matrix
+    }
+    # Create alternative specific design.
+    cte.des <- Altspec(alt.cte = alt.cte, n.sets = n.sets)
+    cte.set <- matrix(cte.des[1:n.alts, ], ncol = n.cte, byrow = FALSE)
+  } else {
+      cte.des <- NULL
+      n.cte <- 0
+      # if no alternative constants 
+      if (!is.matrix(par.draws)) {
+        stop("'par.draws'should be a matrix when 'alt.cte' = NULL")
+      }
+    }
+  
+  # Weights errors
+  n.par <- ncol(par.draws)
+  if (!is.null(weights)) {
+    if (!isTRUE(all.equal(length(weights), nrow(par.draws)))) {
+      stop("length of 'weights' does not match number total number of rows in 'par.draws'")
+    }
+  } else {
+    weights <- rep(1L, nrow(par.draws))
+  }
+  
+  ### Create all levels for each attribute
+  # Continuous attributes. 
+  contins <-  which(coding == "C")
+  n.contins <-  length(contins)
+  # Change into correct coding. 
+  coding <- dplyr::recode(coding, D = "contr.treatment", E = "contr.sum")
+  # Create all combinations of attribute levels.
+  levels.list <- lapply(X = as.list(lvls), function(x) (1:x))
+  # Replace continuous.
+  levels.list[contins] <- c.lvls
+  # Transform to matrix
+  levels.list[contins] <- lapply(X = levels.list[contins], as.matrix)
+  # Transform categorical attributes
+  categ <-  which(coding %in% c("contr.treatment", "contr.sum"))
+  n.categ <- length(categ)
+  if (n.categ > 0) {
+    levels.list[categ] <- lapply(X = levels.list[categ], factor)
+    # Apply coding
+    if (n.contins > 0) {
+      for (i in 1:length(lvls)) {
+        if (!(i %in% contins)) {
+          contrasts(levels.list[[i]]) <- coding[i]
+        }
+      }
+    }else {
+      for (i in 1:length(lvls)) {
+        contrasts(levels.list[[i]]) <- coding[i]
+      }
+    }
+    # Compute all possible values for each categorical attribute
+    levels.list[categ] <- lapply(X = levels.list[categ], contrasts)
+  }
+  # Set colnames for the design matrix
+  c.nam = list()
+  for (i in 1:length(lvls)) {
+    if (coding[i] == "contr.treatment") {
+      c.nam[[i]] <- paste("Var", i, 2:lvls[i], sep = "")
+    } else {
+      if (coding[i] == "contr.sum") {
+        c.nam[[i]] <- paste("Var", i, 1:(lvls[i] - 1), sep = "")
+      } else {
+        c.nam[[i]] <- paste("Var", i, sep = "")
+      }
+    }
+  }
+  c.nam = unlist(c.nam)
+  
+  # Count the number of colums of the design matrix without constants
+  ncol.des.noconst <- sum(unlist(lapply(levels.list,ncol)))
+  
+  # if (!identical(as.integer(n.par), as.integer(n.cte + ncol.des.noconst))) {
+  #   stop("The sum of the number of columns in the components of 'par.draws' should equal the number of columns of design matrix (including alternative specific constants)")
+  # }
+
+  if (!identical(as.integer(ncol(prior.covar)), 
+                            as.integer(n.cte + ncol.des.noconst))) {
+    stop("number of columns of 'prior.covar' does not equal the number of columns of design matrix (including alternative specific constants)")
+  }
+  
+  ## When a design is supplied
+  if (!is.null(des)) {
+    # Error par.draws
+    if (!isTRUE(all.equal(ncol(des), n.par))) {
+      stop("number of columns in 'par.draws' does not match the number of columns in 'des'")
+    }
+    # Error dimension of initial design and design matrix
+    if (!identical(as.integer(ncol(des)), as.integer(n.cte + ncol.des.noconst))) {
+      stop("number of columns in 'des' does not match the number of columns of design matrix (including alternative specific constants)")
+      }
+    # Starting and initializing values.
+    i.cov <- solve(prior.covar)
+    d.start <- apply(par.draws, 1, DerrC_ucpp, des = des, n.alts = n.alts, 
+                     i.cov = i.cov)
+    db.start <- mean(d.start, na.rm = TRUE)
+    full.comb <- Newsets_ucpp(levels.list = levels.list, n.alts = n.alts, 
+                              no.choice = no.choice, reduce = reduce, 
+                              c.names = c.nam, n.cs = n.cs)
+    # Adding alternative constants
+    if (!is.null(cte.des)) {
+      full.comb <- lapply(full.comb, function(x) cbind(cte.set, x))
+    }
+    
+    # Adding these new choice sets to the initial design
+    full.des <- lapply(full.comb, function(x) rbind(des, x))
+    
+    # Select the next best choice set
+    if (parallel) {
+      no_cores <- parallel::detectCores() - 1L
+      cl <- parallel::makeCluster(no_cores)
+      db.errors <- parallel::parLapply(cl, full.des, DBerrS.P_ucpp, par.draws, 
+                                       n.alts, i.cov, weights)
+      parallel::stopCluster(cl)
+    } else {
+      # For each potential set, select best. 
+      db.errors <- lapply(full.des, DBerrS.P_ucpp, par.draws, n.alts, i.cov,
+                          weights)
+    }
+    dbs <- unlist(db.errors, use.names = FALSE)
+    set <- full.comb[[which.min(dbs)]]
+    row.names(set) <- NULL
+    colnames(set) <- colnames(des)
+    db <- min(dbs)
+    #return best set and db error design.
+    return(list("set" = set, "error" = db))
+  } # End if when a design is supplied
+  else {
+    # Starting and initializing values.
+    i.cov <- solve(prior.covar)
+    full.comb <- Newsets_ucpp(levels.list = levels.list, n.alts = n.alts, 
+                              no.choice = no.choice, reduce = reduce, 
+                              c.names = c.nam, n.cs = n.cs)
+    # Adding alternative constants
+    if (!is.null(cte.des)) {
+      full.comb <- lapply(full.comb, function(x) cbind(cte.set, x))
+    }
+    # Select the next best choice set
+    if (parallel) {
+      no_cores <- parallel::detectCores() - 1L
+      cl <- parallel::makeCluster(no_cores)
+      db.errors <- parallel::parLapply(cl, full.comb, DBerrS.P_ucpp, par.draws, 
+                                       n.alts, i.cov, weights)
+      parallel::stopCluster(cl)
+    } else {
+      # For each potential set, select best. 
+      db.errors <- lapply(full.comb, DBerrS.P_ucpp, par.draws, n.alts, i.cov,
+                          weights)
+    }
+    dbs <- unlist(db.errors, use.names = FALSE)
+    set <- full.comb[[which.min(dbs)]]
+    row.names(set) <- NULL
+    if (n.cte > 0) {
+      colnames(set) <- c(paste("alt",1:n.cte,".cte", sep = ""), c.nam)
+    }
+    db <- min(dbs)
+    #return best set and db error design.
+    return(list("set" = set, "error" = db))
+  }
+}
--- R/Fullsets_ucpp.R
+++ R/Fullsets_ucpp.R
@@ -1,21 +1,65 @@
 # Fullsets using Infodes_cpp
 Fullsets_ucpp <- function(cand.set, n.alts, no.choice, reduce = TRUE){
-  
-  if(!is.null(no.choice)){
+  if (!is.null(no.choice)) {
     n.alts <- n.alts - 1
   }
-  full.comb <- utils::combn(1:nrow(cand.set), n.alts, FUN = function(x)  cand.set[x, ], simplify = FALSE)
+  full.comb <- utils::combn(1:nrow(cand.set), n.alts, 
+                            FUN = function(x)  cand.set[x, ], simplify = FALSE)
   #reduce
-  if (reduce){
+  if (reduce) {
     m <- stats::rnorm(ncol(cand.set))
-    inf <-list()
-    for(i in 1:length(full.comb)){
+    inf <- list()
+    for (i in 1:length(full.comb)) {
       inf[[i]] <- round(InfoDes_cpp(m, full.comb[[i]], n.alts), digits = 3)
     }
     t <- array(unlist(inf), dim = c(length(m), length(m), length(inf))) 
     full.comb <- full.comb[!duplicated(t, MARGIN = 3)]
   }
-  if(!is.null(no.choice)){
+  if (!is.null(no.choice)) {
+    full.comb <- lapply(full.comb, Inchoice, no.choice = no.choice)
+  }
+  return(full.comb)
+}
+
+
+# This function is the equivalent of Fullsets for CEA algorithm. Instead
+# of computing all possible combinations of alternatives, this new function
+# creates alternatives by randomly choosing the level of each attribute
+# n.cs: is the number of random choice sets to 
+# c.names: is the column names of the design matrix
+Newsets_ucpp <- function(levels.list, n.alts, no.choice, reduce = TRUE, 
+                         n.cs = NULL, c.names = NULL){
+  if (!is.null(no.choice)) {
+    n.alts <- n.alts - 1
+  }
+  if (is.null(n.cs)) {
+    n.cs <- prod(unlist(lapply(levels.list, nrow)))
+  }
+  # Generation of random choice sets
+  full.comb <- vector(mode = 'list', length = n.cs)
+  for (i in 1:n.cs) {
+    # r is to know which levels to take in each attribute
+    r <- NULL
+    cs <- NULL
+    for (j in 1:length(levels.list)) {
+      r <- round(stats::runif(n.alts, 1, nrow(levels.list[[j]])))
+      cs <- cbind(cs, levels.list[[j]][r,])
+    }
+    colnames(cs) <- c.names
+    rownames(cs) <- NULL
+    full.comb[[i]] <- cs
+  } # end loop i
+  #reduce
+  if (reduce) {
+    m <- stats::rnorm(ncol(full.comb[[1]]))
+    inf <- list()
+    for (i in 1:length(full.comb)) {
+      inf[[i]] <- round(InfoDes_cpp(m, full.comb[[i]], n.alts), digits = 3)
+    }
+    t <- array(unlist(inf), dim = c(length(m), length(m), length(inf))) 
+    full.comb <- full.comb[!duplicated(t, MARGIN = 3)]
+  }
+  if (!is.null(no.choice)) {
     full.comb <- lapply(full.comb, Inchoice, no.choice = no.choice)
   }
   return(full.comb)
